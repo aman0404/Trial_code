64e9de875af927e4aef56d5f224541e2068f953e
aman0404 <amey0404@yahoo.com>
2018-09-19 00:01:12 +0530

    Add files via upload

---
+++ fake_varPt.C
@@ -0,0 +1,94 @@
+{
+
+TCanvas *c1 = new TCanvas("c1", "stacked hists",61,24,1305,744);
+   c1->Range(-31.19002,-0.07272727,281.4299,0.6525253);
+   c1->SetFillColor(0);
+   c1->SetBorderMode(0);
+   c1->SetBorderSize(2);
+   c1->SetTopMargin(0.0724234);
+   c1->SetFrameBorderMode(0);
+   c1->SetFrameBorderMode(0);
+
+Double_t bins[15] = {0,10,20,30,40,50,60,70,80,90,100,120,140,170,200};
+
+TH1 *hloose,*htight, *hloosebkg, *htightbkg;
+
+
+
+TFile *f1 = TFile::Open("14Sept/looseIso_2D/Data.root");
+f1->GetObject("NRecoTau1/FirstLeadingTau1Pt",hloose);
+TFile *f2 = TFile::Open("14Sept/tightIso_2D/Data.root");
+f2->GetObject("NRecoTau1/FirstLeadingTau1Pt",htight);
+
+TFile *f3 = TFile::Open("14Sept/looseIso_2D/bkg.root");
+f3->GetObject("NRecoTau1/FirstLeadingTau1Pt",hloosebkg);
+TFile *f4 = TFile::Open("14Sept/tightIso_2D/bkg.root");
+f4->GetObject("NRecoTau1/FirstLeadingTau1Pt",htightbkg);
+
+
+
+TH1F *h1 = (TH1F*)htight->Rebin(14,"h1",bins);
+TH1F *h2 = (TH1F*)hloose->Rebin(14,"h2",bins);
+
+TH1F *h3 = (TH1F*)htightbkg->Rebin(14,"h3",bins);
+TH1F *h4 = (TH1F*)hloosebkg->Rebin(14,"h4",bins);
+
+//TH1F *h1 = (TH1F*)htight->Clone("h1");
+
+
+
+/*
+TH1F *h2 = (TH1F*)hloose->Clone("h2");
+
+TH1F *h3 = (TH1F*)htightbkg->Clone("h3");
+TH1F *h4 = (TH1F*)hloosebkg->Clone("h4");
+
+
+h1->Rebin(10);
+h2->Rebin(10);
+
+h3->Rebin(10);
+h4->Rebin(10);
+*/
+
+h1->Add(h3,-1);
+h2->Add(h4,-1);
+
+h1->Divide(h2);
+h1->SetStats(kFALSE);
+
+h1->Draw("ep");
+h1->SetMarkerStyle(22);
+h1->GetXaxis()->SetRangeUser(0,250);
+h1->GetYaxis()->SetRangeUser(0,0.6);
+h1->SetMarkerSize(1.4);
+h1->SetTitle("");
+h1->GetXaxis()->SetTitle("#tau_{h} P_{T} [GeV]");
+h1->GetYaxis()->SetTitle("#frac{tight}{loose}");
+   h1->SetLineWidth(2);
+   h1->SetLineColor(kBlack);
+
+
+
+
+
+TLatex *   tex = new TLatex(17.19589,0.5566778,"CMS");
+   tex->SetTextAlign(20);
+   //tex->SetTextSize(0.045);
+   tex->SetLineWidth(2);
+   tex->Draw();
+TLatex *   tex1 = new TLatex(25.11032,0.5246717,"#it{#bf{work in progress}}");
+   tex1->SetTextAlign(20);
+   tex1->SetTextSize(0.02898551);
+
+   tex1->SetLineWidth(2);
+   tex1->Draw();
+
+c1->SaveAs("plots/JetpT_dataDrivenDY_var_14Sept.root");
+c1->SaveAs("plots/JetpT_dataDrivenDY_var_14Sept.png");
+
+
+}
+
+
+
